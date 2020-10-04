8865bb56950ce12415db4eb9a18baa29ffa0f815
shizhifan <2219762131@qq.com>
2020-10-04 14:44:10 +0800

    work1

---
+++ precache-manifest.b634d90e92508953bb2ff21182909959.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f80390561bcc730df5b1593f930260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544293b9a0bc2339cc24",
+    "url": "/static/js/main.cebaf82b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
